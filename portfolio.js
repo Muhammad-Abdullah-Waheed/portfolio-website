17edd2b532d6dbd073128c89934c3fa17ae792b0
Muhammad Abdullah Waheed <mabdullahwaheed127@gmail.com>
2024-09-21 17:59:36 +0500

    Project Page Dynamically populated

---
+++ portfolio.js
@@ -0,0 +1,58 @@
+const portfolioData = {
+  projects: [
+    {
+      title: "Text Files Search Engine",
+      startDate: "December 2023",
+      endDate: "December 2023",
+      videoLink: "https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7147643683901812738",
+    },
+    {
+      title: "Interactive Disk Operating System and Text Editor",
+      startDate: "November 2023",
+      endDate: "December 2023",
+      videoLink: "https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7149376215827599360",
+    },
+    {
+      title: "Chess Game with SFML",
+      startDate: "October 2023",
+      endDate: "November 2023",
+      videoLink: "https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7146117299970764800",
+    },
+    {
+      title: "Ludo Game with SFML",
+      startDate: "September 2023",
+      endDate: "October 2023",
+      videoLink: "https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7146122679765438464",
+    },
+    {
+      title: "Paint Brush Application",
+      startDate: "May 2023",
+      endDate: "June 2023",
+      videoLink: "https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7146212725994541056",
+    },
+    {
+      title: "Mini Excel Project in C++",
+      startDate: "April 2023",
+      endDate: "May 2023",
+      videoLink: "https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7149369489959153665",
+    },
+    {
+      title: "Shogi Game in C++",
+      startDate: "November 2022",
+      endDate: "December 2022",
+      videoLink: "https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7146074766964174848",
+    },
+    {
+      title: "C++ Console Snake Game",
+      startDate: "Feb 2023",
+      endDate: "March 2023",
+      videoLink: "https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7146076699863027714",
+    },
+    {
+      title: "C++ Solitaire Game with SFML",
+      startDate: "January 2023",
+      endDate: "March 2023",
+      videoLink: "https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7146130739762307072",
+    },
+  ],
+};
